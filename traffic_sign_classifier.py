6fb51e2be0d556354a9ea9c1ea1b7afccefb0e1c
olasson <oystein.lasson@gmail.com>
2020-12-22 23:34:16 +0100

    Showing images

--- traffic_sign_classifier.py
+++ traffic_sign_classifier.py
@@ -1,5 +1,8 @@
 from code.io import data_load_pickled, data_save_pickled
+from code.helpers import images_pick_subset
+from code.show import show_images
 
+from pandas import read_csv
 import numpy as np
 import argparse
 
@@ -48,19 +51,27 @@ def main():
     )
 
     parser.add_argument(
-        '--random',
-        action = 'store_true',
-        help = 'Can be paired with [show_images] to cause different behavior.'
+        '--show_images_max',
+        type = int,
+        default = 25,
+        help = 'Maximum number of images displayed.'
     )
 
     args = parser.parse_args()
 
-    # ---------- Pack arguments ---------- #
+    # ---------- Setup ---------- #
 
     data_paths = np.array([args.data_train, args.data_valid, args.data_test])
 
     flag_show_images = args.show_images
-    flag_random = args.random
+
+    n_images_max = args.show_images_max
+
+    try:
+        y_metadata = read_csv('signnames.csv')['SignName']
+    except FileNotFoundError:
+        y_metadata = None
+        print("No metadata file found!")
 
     # ---------- Show data ---------- #
 
@@ -68,8 +79,13 @@ def main():
 
         for data_path in data_paths:
             if file_exists(data_path):
-                print("Loading data set:", data_path, "...")
+                
                 X, y = data_load_pickled(data_path)
+
+                # Pick out a subset of images if 'len(X) > n_images_max'
+                X_sub, y_sub, y_metadata_sub = images_pick_subset(X, y, y_metadata, n_images_max = n_images_max)
+
+                show_images(X_sub, y_sub, y_metadata_sub, title_window = data_path)
 
 
 
