c988d464e798bc5eb698eb1b06b364775b2f0f66
olasson <oystein.lasson@gmail.com>
2020-12-22 17:26:13 +0100

    Added command line arguments

--- traffic_sign_classifier.py
+++ traffic_sign_classifier.py
@@ -3,6 +3,33 @@ import os
 
 
 def main():
-    pass
+
+    # ---------- Command line arguments ---------- #
+
+    parser = argparse.ArgumentParser(description = 'Detect Lane Lines')
+
+    parser.add_argument(
+        '--show_images',
+        type = str,
+        nargs = '?',
+        default= '',
+        help = 'Show a set of images.'
+    )
+
+    parser.add_argument(
+        '--random',
+        type = bool,
+        default = False,
+        help = 'Can be paired with [--show_images] to cause different behavior.'
+    )
+
+    args = parser.parse_args()
+
+    if args.show_images:
+        print("Test")
+
+        if args.random:
+            print("Test2")
+
 
 main()
