829f1a961dfd2567f67730d057e9e52bddd89b42
olasson <oystein.lasson@gmail.com>
2020-12-24 13:04:18 +0100

    Lots of updates

--- traffic_sign_classifier.py
+++ traffic_sign_classifier.py
@@ -1,6 +1,8 @@
+from code.prepare import augment_data_by_mirroring
+from code.show import show_images, show_label_distributions
 from code.io import data_load_pickled, data_save_pickled
 from code.helpers import images_pick_subset
-from code.show import show_images, show_label_distributions
+
 
 from pandas import read_csv
 import numpy as np
@@ -8,9 +10,27 @@ import argparse
 
 from os.path import exists as file_exists
 
+
+# Metadata info
 PATH_METADATA = 'signnames.csv'
 KEY_METADATA = 'SignName'
 
+# Fixed names for prepared data
+PATH_PREPARED_FOLDER = './data/'
+PATH_PREPARED_TRAIN = PATH_PREPARED_FOLDER + 'prepared_train.p'
+PATH_PREPARED_VALID = PATH_PREPARED_FOLDER + 'prepared_valid.p'
+PATH_PREPARED_TEST = PATH_PREPARED_FOLDER + 'prepared_test.p'
+
+# Permitted options for --prepare command
+DATA_PREPARATION_OPTIONS = ['mirroring', 'rand_tf', 'hist_eq']
+
+# Mapping where "Class i" is mirrored to imitate "Class MIRROR_MAP[i]"
+# Used by the --prepare command
+MIRROR_MAP = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+              -1, 11, 12, 13, -1, 15, -1, 17, 18, 20,
+              19, -1, 22, -1, -1, -1, 26, -1, -1, -1,
+              30, -1, -1, 34, 33, 35, 37, 36, 39, 38,
+              -1, -1, -1]
 
 def main():
 
@@ -78,6 +98,12 @@ def main():
         help = 'Determines which distribution will be used to set the class order on the y-axis.'
     )
 
+    parser.add_argument(
+        '--prepare',
+        type = str,
+        nargs = '*',
+        help = 'Initiates data preparation (augmentation + pre-processing). If no flags are provided, all steps are performed.'
+    )
 
 
     args = parser.parse_args()
@@ -90,6 +116,23 @@ def main():
 
     flag_show_images = args.show_images
     flag_show_distributions = args.show_distributions
+
+    if args.prepare is not None:
+
+        flag_prepare = True
+
+        if len(args.prepare) > 0:
+            flag_mirroring = DATA_PREPARATION_OPTIONS[0] in args.prepare
+            flag_random_transform = DATA_PREPARATION_OPTIONS[1] in args.prepare
+            flag_histogram_equalization = DATA_PREPARATION_OPTIONS[2] in args.prepare
+        else: 
+            flag_mirroring = True
+            flag_random_transform = True
+            flag_histogram_equalization = True
+
+    else:
+        flag_prepare = False
+
 
     n_images_max = args.n_images_max
 
@@ -154,6 +197,23 @@ def main():
     if flag_show_distributions:
         show_label_distributions([y_train, y_test, y_valid], y_metadata, title = title_distributions, order_index = order_index)
 
+
+    # ---------- Prepare data ---------- #
+    if flag_prepare:
+
+    # Prepare training data
+
+        if flag_mirroring:
+            print("MIRRORING")
+
+        if flag_random_transform:
+            print("RAND TRANSFORMS")
+
+
+    # Pre-processing
+
+        if flag_histogram_equalization:
+            print("HIST EQ")
 
 
 
