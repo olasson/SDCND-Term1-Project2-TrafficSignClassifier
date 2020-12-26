ba34d8561c0a22c561b9b611f1edce8453e6f549
olasson <oystein.lasson@gmail.com>
2020-12-26 10:20:47 +0100

    Added VGG16 model

--- code/models.py
+++ code/models.py
@@ -8,6 +8,7 @@ from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPooling2D, Activation, Flatten, Dropout
 from tensorflow.keras.layers import Dense
 
+# LeNet
 
 def LeNet():
     """
@@ -59,4 +60,81 @@ def LeNet():
     model.add(Dense(43))
     model.add(Activation("softmax"))
 
+    return model
+
+# VGG16
+
+def VGG16():
+    """
+    Create a VGG16 inspired model
+    
+    Inputs
+    ----------
+        N/A
+        
+    Outputs
+    -------
+    model : tf.keras.Sequential
+        Custom model
+
+    """
+
+    model = Sequential()
+    norm_axis = -1
+    
+    # Conv Layer: 32x32x1 -> 32x32x8.
+    model.add(Conv2D(filters = 8, kernel_size = (5,5), padding = "same", input_shape = (32, 32, 1)))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization(axis = norm_axis))
+
+    # Layer: 32x32x1 -> 16x16x8.
+    model.add(MaxPooling2D(pool_size = (2, 2)))
+    
+    # Conv Layer: 16x16x8 -> 16x16x16
+    model.add(Conv2D(filters = 16, kernel_size = (3,3), padding = "same"))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization(axis = norm_axis))
+    model.add(Conv2D(filters = 16, kernel_size = (3,3), padding = "same"))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization(axis = norm_axis))
+
+    # Layer: 16x16x8 -> 8x8x16
+    model.add(MaxPooling2D(pool_size = (2,2)))
+    
+    # Layer: 8x8x16 -> 8x8x32
+    model.add(Conv2D(filters = 32, kernel_size = (3,3), padding = "same"))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization(axis = norm_axis))
+    model.add(Conv2D(filters = 32, kernel_size = (3,3), padding = "same"))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization(axis = norm_axis))
+    model.add(Conv2D(filters = 32, kernel_size = (3,3), padding = "same"))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization(axis = norm_axis))
+
+    # Layer: 8x8x32 -> 4x4x32
+    model.add(MaxPooling2D(pool_size = (2,2)))
+    
+    # Flatten Layer: 4x4x32 -> 512
+    model.add(Flatten())
+
+    # FC Layer: 512 -> 128
+    model.add(Dense(128))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.7))
+    
+    # Flatten Layer:: 128 -> 128
+    model.add(Flatten())
+
+    # FC Layer: 128 -> 128
+    model.add(Dense(128))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+    
+    # FC Layer: 128 -> 43
+    model.add(Dense(43))
+    model.add(Activation("softmax"))
+    
     return model
